a627f8464518f39004ed1e2b07ca3c421bba793d
raulanatol <raul@natol.es>
2021-04-20 18:36:23 +0100

    Timezone

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const dayjs = require('dayjs');
 const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
 
 dayjs.extend(utc);
+dayjs.extend(timezone);
 
 module.exports = {
   getValue: () => {
